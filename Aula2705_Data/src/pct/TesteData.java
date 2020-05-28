61c80cfc9bd45d6743ac1130a0970a0d40c658f6
ANTONIO <ANTONIO@10.0.0.105>
2020-05-27 22:25:45 -0300

    Commit Roberta, set/get

--- Aula2705_Data/src/pct/TesteData.java
+++ Aula2705_Data/src/pct/TesteData.java
@@ -14,6 +14,17 @@ public class TesteData {
         //Criação e instância de um objeto da classe data
         Data data = new Data();
         
+        //contrucao
+        data.setDia (4);
+        data.setMes(6);
+        data.setAno(2020);
+        
+        //saida de dados
+        System.out.println("Data: "+ data.imprimirData());
+        
+        //saida de dados
+        System.out.println("Dia: "+ data.getDia());
+        
         
     }
 }
